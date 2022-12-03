5bfbe630766e0cf37daa0bc2ff1197037cafbdc9
UAndreiRamescu <andrei.ramescu@uqido.com>
2022-12-03 02:15:13 +0100

    Another one

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/andreiramescu/Developer/Flutter/SDK/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/andreiramescu/IdeaProjects/storytells"
+export "FLUTTER_ROOT=/Users/andreiramescu/fvm/versions/3.3.5"
+export "FLUTTER_APPLICATION_PATH=/Users/andreiramescu/Developer/Personal/storytells"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/andreiramescu/Developer/Personal/storytells/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/andreiramescu/Developer/Personal/storytells/.dart_tool/package_config.json"
